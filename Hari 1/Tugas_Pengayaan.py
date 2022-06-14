a324bbed754c1b464833ce9df99cf3501d5a5c50
mihoku <49892705+mihoku@users.noreply.github.com>
2022-06-14 19:27:51 +0700

    Update Tugas_Pengayaan.py

--- Hari 1/Tugas_Pengayaan.py
+++ Hari 1/Tugas_Pengayaan.py
@@ -1,14 +1,21 @@
 #Tugas Pengayaan
-#Write a program that converts a binary (base 2) number to decimal (base 10). 
-#Your program should begin by reading the binary number from the user as a string. 
-#Then it should compute the equivalent decimal number by processing each digit in the binary number. 
-#Finally, your program should display the equivalent decimal number with an appropriate message.
 
 binary_number = input("Bilangan biner yang ingin dikonversi ke decimal: ")
+
+#Teknik 1: string slicing dari kiri
 decimal_conversion = 0
 for urutan_karakter in range(len(binary_number)):
-  karakter = int(binary_number[urutan_karakter:urutan_karakter+1])
+  karakter = int(binary_number[urutan_karakter])
   pangkat = len(binary_number) - urutan_karakter - 1
   decimal_conversion = decimal_conversion+karakter*2**pangkat
 
-print("Bilangan biner %s jika dikonversi ke desimal menjadi %d" %(binary_number,decimal_conversion))
+print("Bilangan biner %s jika dikonversi ke desimal menjadi %d (Teknik 1)" %(binary_number,decimal_conversion))
+
+#Teknik 2: string slicing dari kanan
+decimal_conversion = 0
+for urutan_karakter in range(len(binary_number)):
+  karakter = int(binary_number[-urutan_karakter-1])
+  pangkat = urutan_karakter
+  decimal_conversion = decimal_conversion+karakter*2**pangkat
+
+print("Bilangan biner %s jika dikonversi ke desimal menjadi %d (Teknik 2)" %(binary_number,decimal_conversion))
