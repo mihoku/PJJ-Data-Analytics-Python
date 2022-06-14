79a0e51f597761e6d596ef4bc7b584d3607c1215
mihoku <49892705+mihoku@users.noreply.github.com>
2022-06-14 19:10:38 +0700

    Update Tugas_6.py

--- Hari 1/Tugas_6.py
+++ Hari 1/Tugas_6.py
@@ -6,8 +6,10 @@
 #[Hint: Use the // and % operators to separate the number into its digits.]
 
 supposedly_palindrome = input("Coba input bilangan palindrome: ")
-bilangan_uji = int(supposedly_palindrome)
 panjang_input = len(supposedly_palindrome)
+
+#Teknik 1: menggunakan operator // dan %
+bilangan_uji = int(supposedly_palindrome)
 result = True
 
 for i in range(panjang_input):
@@ -20,6 +22,27 @@ for i in range(panjang_input):
   result = result and test
 
 if(result):
-  print("Bilangan %s merupakan palindrome"%supposedly_palindrome)
+  print("Bilangan %s merupakan palindrome (Teknik 1)"%supposedly_palindrome)
 else:
-  print("Bilangan %s bukan merupakan palindrome"%supposedly_palindrome)
+  print("Bilangan %s bukan merupakan palindrome (Teknik 1)"%supposedly_palindrome)
+
+#Teknik 2: menggunakan string slicing
+result2 = True
+
+for a in range(panjang_input):
+  digit_depan = supposedly_palindrome[a]
+  digit_belakang = supposedly_palindrome[-a-1]
+  test2 = digit_depan==digit_belakang
+  result2 = result2 and test2
+
+if(result2):
+  print("Bilangan %s merupakan palindrome (Teknik 2)"%supposedly_palindrome)
+else:
+  print("Bilangan %s bukan merupakan palindrome (Teknik 2)"%supposedly_palindrome)
+
+#Teknik 3: string inverse
+test3 = supposedly_palindrome[::-1]==supposedly_palindrome
+if(test3):
+  print("Bilangan %s merupakan palindrome (Teknik 3)"%supposedly_palindrome)
+else:
+  print("Bilangan %s bukan merupakan palindrome (Teknik 3)"%supposedly_palindrome)
